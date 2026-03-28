bd603f82fbf419874a8a2945b31d0da7dff9d172
igorrm23 <igormachadofire14@gmail.com>
2026-03-28 03:12:30 -0300

    refactor: update CardLogin styling to a gradient background and remove unused window icon

--- frontend/src/components/cardLogin.tsx
+++ frontend/src/components/cardLogin.tsx
@@ -1,6 +1,13 @@
 function CardLogin({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full max-w-md md:max-w-lg rounded-xl shadow-2xl p-6 md:p-10 border transition-colors duration-300 bg-white border-zinc-200 dark:bg-zinc-900 dark:border-zinc-800 flex flex-col items-center justify-center">
+        <div className=" w-full max-w-md md:max-w-lg rounded-xl p-6 md:p-10 border
+  bg-gradient-to-b 
+  from-[#2A0A4A] 
+  via-[#2F0D55] 
+  to-[#1F073A]
+  border-zinc-200/10
+  shadow-2xl
+  flex flex-col items-center justify-center">
             {children}
         </div>
     );
